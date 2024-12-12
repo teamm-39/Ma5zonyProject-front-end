99b217dd7a9a58eb7a61638017e02d0cf7d91841
teamm-39 <eslam.ex2003@gmail.com>
2024-12-12 18:55:39 +0200

    create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/auth/LoginPage.jsx'
-import './assets/css/global.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css';  // الموضوع
-import 'primereact/resources/primereact.min.css';  // الأسلوب العام للمكونات
-import 'primeicons/primeicons.css';  // أيقونات PrimeReact
-import { PrimeReactProvider } from 'primereact/api';
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import "./assets/css/global.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // الموضوع
+import "primereact/resources/primereact.min.css"; // الأسلوب العام للمكونات
+import "primeicons/primeicons.css"; // أيقونات PrimeReact
+import { PrimeReactProvider } from "primereact/api";
+import NavBar from "./features/NavBar.jsx";
 
 function App() {
   return (
     <>
       <PrimeReactProvider>
-      <Routes >
-      <Route path='/Login' element={<LoginPage />} />
-    </Routes>
+        <Routes>
+          <Route path="/Login" element={<LoginPage />} />
 
+        </Routes>
+        <NavBar />
       </PrimeReactProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/features/NavBar.jsx
@@ -0,0 +1,42 @@
+function NavBar() {
+  return (
+    <div className="card">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Logo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav text-center">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+              <a className="nav-link" href="#">
+                Features
+              </a>
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
