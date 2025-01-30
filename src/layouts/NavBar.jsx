2633fd95ad28086d684444ab49d6f012fe5668eb
teamm-39 <eslam.ex2003@gmail.com>
2025-01-30 15:50:37 +0200

    style navbar

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,11 +1,19 @@
+import "../assets/css/navbar.css";
+import logo from '../assets/imgs/baner/login-logo.png';
+import baner from '../assets/imgs/baner/login-buner.png';
+import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="nav-container">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Logo
-          </a>
+          <Link className="navbar-brand d-flex align-items-center pe-3 p-0 m-0 gap-1" to="/">
+            <img src={logo} alt="" width={40} style={{height:"50px"}} />
+            <div className="brand-name text-center d-flex flex-column">
+              <h2 className="fw-bold p-0 m-0 text-light">مخزون</h2>
+              <span className="brand-span m-0 p-0 text-light">ادارة المستودعات والمخازن</span>
+            </div>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -17,20 +25,15 @@ function NavBar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav text-center">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-              <a className="nav-link" href="#">
-                Features
-              </a>
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
+          <div className="collapse navbar-collapse border-end border-2 me-4 d-flex justify-content-end" id="navbarNavAltMarkup">
+            <div className="navbar-nav border-end pe-4 border-2 ps-3">
+              <div className="d-flex profile-box gap-2 align-items-center">
+                <img src={baner} alt="profile pic" width={50} />
+                <div className="profile-content d-flex flex-column">
+                  <h6 className="m-0 p-0 fw-bold">احمد على</h6>
+                  <span>ادمن</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
