ac5f366af64856580b4d9011977e736ae518cef7
teamm-39 <eslam.ex2003@gmail.com>
2025-04-16 00:57:37 +0200

    Update API URL in isLogedIn service to use environment variable for improved configuration management

--- src/features/auth/services/IsLogedin.js
+++ src/features/auth/services/IsLogedin.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const isLogedIn = async () => {
   try {
-    const response = await axios.get('https://localhost:7213/api/Users/is-loged-in',{withCredentials:true});
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}Users/is-loged-in`,{withCredentials:true});
     return response.data;
   } catch (e) {
     console.log(e);
