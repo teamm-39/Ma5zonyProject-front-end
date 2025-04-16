7c2aca177822d078c3ded6974092b65a2c06aee9
teamm-39 <eslam.ex2003@gmail.com>
2025-04-16 03:25:22 +0200

    Refactor login and getStores services to ensure withCredentials is set for improved session management

--- src/features/auth/services/login.js
+++ src/features/auth/services/login.js
@@ -5,9 +5,7 @@ export const login = async (email, password) => {
     const response = await axios.post(`${import.meta.env.VITE_API_URL}Users/sign-in`, {
       email,
       password
-    },{
-      withCredentials: true // Include cookies
-  });
+    });
     return response.data;
   } catch (e) {
     console.log(e);
