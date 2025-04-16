8d537783bff5d9840a2774fc7046a49a27442cbe
teamm-39 <eslam.ex2003@gmail.com>
2025-04-16 04:44:57 +0200

    Refactor isLogedIn and login services to ensure withCredentials is set for improved session management

--- src/features/auth/services/IsLogedin.js
+++ src/features/auth/services/IsLogedin.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 export const isLogedIn = async () => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}Users/is-loged-in`, { withCredentials: true });
-    return response.data; // أعد البيانات المستلمة من الخادم
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}Users/is-loged-in`,{withCredentials:true});
+    return response.data;
   } catch (e) {
-    console.error(e); // سجل الخطأ للمراجعة
-    return { data: false }; // أعد قيمة واضحة عند حدوث الخطأ (لتجنب undefined)
+    console.log(e);
   }
 };
--- src/features/auth/services/login.js
+++ src/features/auth/services/login.js
@@ -5,7 +5,7 @@ export const login = async (email, password) => {
     const response = await axios.post(`${import.meta.env.VITE_API_URL}Users/sign-in`, {
       email,
       password
-    });
+    },{withCredentials:true});
     return response.data;
   } catch (e) {
     console.log(e);
